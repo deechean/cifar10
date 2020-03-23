7f60edeaa1b94071b3479e5d945dac260f1636d1
ubuntu <deechean@hotmail.com>
2020-03-23 23:34:44 +0800

    add random_distort function

--- cifar10.py
+++ cifar10.py
@@ -20,13 +20,16 @@ def load(file_name):
         return data  
 
 class cifar10(object): 
-    def __init__(self,size = 32, path='cifar-10-batches-py/'):
+    def __init__(self,size = 32, path='cifar-10-batches-py/', ramdom_wrap=True, random_flip=True, random_distort=True):
         self.train_indexs = list()
         self.test_indexs = list()
         self.data_path = path
         self.train_images, self.train_labels = self._get_train()
         self.test_images, self.test_labels = self._get_test()
         self.image_size = size
+        self.wrap = ramdom_wrap
+        self.flip = random_flip
+        self.distort = random_distort
         self.label_dic = {0:'aircraft', 1:'car',2:'bird',3:'cat',4:'deer',5:'dog',6:'frog',7:'horse',8:'ship',9:'truck'}
 
         
@@ -93,57 +96,38 @@ class cifar10(object):
         #print(resized_image.shape)
         return resized_image
     
-    def random_flipper(self,image):
+    def random_flip(self,image):
         if random.random() < 0.5:
             image = np.flipud(image)
-        return image
+        return image.astype('uint8')
     
-    # 预处理函数
-    # im中的像素值为 [0, 255] 闭区间， 则 (1+im) 为 [1, 256]
-    # 先做 (1+im)/257 操作将值归一化到 (0, 1) 开区间内
-    # 再使用 sigmoid函数 的反函数，效果见sigmod函数图像
-    # -np.log(1/((1 + 0)/257) - 1) = -5.5451774444795623
-    # -np.log(1/((1 + 255)/257) - 1) = 5.5451774444795623
-    def do_normalise(self,im):
-        return -np.log(1/((1 + im)/257) - 1)  
-    
-    # 预处理函数的反函数
-    # 即先使用sigmod函数，再将值变换到(0, 257)区间再减1，通过astype保证值位于[0, 255]
-    # 关于 astype("uint8") ：
-    # np.array([-1]).astype("uint8") = array([255], dtype=uint8)
-    # np.array([256]).astype("uint8") = array([0], dtype=uint8)
-    def undo_normalise(self,im):
-        return (1/(np.exp(-im) + 1) * 257 - 1).astype("uint8")
-    
-    def rotation_matrix(self,theta):
-        """
-        3D 旋转矩阵，围绕X轴旋转theta角
-        """
-        return np.c_[
-            [1,0,0],
-            [0,np.cos(theta),-np.sin(theta)],
-            [0,np.sin(theta),np.cos(theta)]
-            ]
-    
-    
-    def distort_color(self,image):
+    def random_distort(self,image):
         if random.random() < 0.5:
-            image = self.do_normalise(image)
-            image = image.transpose((1,2,0))
-            image = np.einsum('ijk,lk->ijl', image, self.rotation_matrix(np.pi/4))
-            # 利用爱因斯坦求和约定做矩阵乘法，实际上是将每个RGB像素点表示的三维空间点绕X轴（即红色通道轴）旋转180°。
-            image = image.transpose((2,0,1))
-            image = self.undo_normalise(image)            
-        return image
-        
+            x = random.randint(1,10)
+            y = random.randint(1,10)
+            image = self.resize_image(image, (self.image_size+x,self.image_size+y))
+            image = image[:, :self.image_size, :self.image_size]
+        return image.astype('uint8')
     
     def random_bright(self, image, delta=32):      
         if random.random() < 0.3:   
             image = image/255.0
             image = np.power(image,0.5)*255
-        return image
-   
-    def get_train_batch(self,batch_size=128, augument = True):
+        return image.astype('uint8')
+    
+    def resize_image(self, image, new_size):
+        img = Image.fromarray(image.transpose(1,2,0))       
+        return np.asarray(img.resize(new_size,resample=Image.BILINEAR)).transpose(2,0,1)
+    
+    def ramdom_wrap(self, image):        
+        if random.random() < 0.5:
+            max_offsetx = random.randint(1,8)
+            max_offsety = random.randint(1,8)
+            image = image[:,max_offsetx:-max_offsetx,max_offsety:-max_offsety]
+            image = self.resize_image(image, (self.image_size,self.image_size))
+        return image.astype('uint8')
+    
+    def get_train_batch(self,batch_size=128):
         batch_image = list()
         batch_label = list()
         data_index = list()
@@ -153,11 +137,15 @@ class cifar10(object):
             if not index in self.train_indexs:
                 i += 1
                 d = self.train_images[index]
-                if augument:
-                    #d = self.ramdom_wrap(self.random_bright(self.random_flipper(d)))
-                    #d = self.random_flipper(d)
-                    d = self.ramdom_wrap(self.random_flipper(d))
-                    d = d.astype('uint8')
+                
+                if self.wrap == True:
+                    d = self.ramdom_wrap(d)
+ 
+                if self.flip == True:
+                    d = self.random_flip(d) 
+            
+                if self.distort == True:
+                    d = self.random_distort(d) 
                     
                 batch_image.append(d)
                 batch_label.append(self.train_labels[index])
@@ -185,14 +173,4 @@ class cifar10(object):
                     self.test_indexs.clear()
         return  np.array(batch_image).transpose(0,3,2,1).reshape(-1,32,32,3), batch_label,data_index    
     
-    def resize_image(self, image, new_size):
-        img = Image.fromarray(image.transpose(1,2,0))       
-        return np.asarray(img.resize(new_size,resample=Image.BILINEAR)).transpose(2,0,1)
     
-    def ramdom_wrap(self, image):        
-        if random.random() < 0.5:
-            max_offsetx = random.randint(1,8)
-            max_offsety = random.randint(1,8)
-            image = image[:,max_offsetx:-max_offsetx,max_offsety:-max_offsety]
-            image = self.resize_image(image, (self.image_size,self.image_size))
-        return image
